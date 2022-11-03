2ed4bcb735f8986535142f1d53b19cbc87e2a1be
mdathar <mdathar4403@gmail.com>
2022-11-03 10:18:22 +0530

    ainmation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,18 +3,32 @@ import './header.css';
 import CTA from "./CTA";
 import ME from "../../assets/me.png"
 import HeaderSocial from "./HeaderSocial";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
-function Header(){
+
+function Header() {
     return (
         <header>
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>Md Athar</h1>
                 <h5 className="text-light">Full Stack Developer</h5>
-                <CTA/>
-                <HeaderSocial/>
+                <CTA />
+                <HeaderSocial />
 
-                <div className="me">
+                <div className="me"
+                    data-aos="fade-up"
+                    data-aos-offset="100"
+                    data-aos-delay="30"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out"
+                    data-aos-mirror="true"
+                    data-aos-once="false"
+                    data-aos-anchor-placement="top-center"
+
+                >
                     <img src={ME} alt="" />
                 </div>
 
@@ -22,7 +36,7 @@ function Header(){
 
             </div>
         </header>
-        
+
     )
 }
 
